4de5082c7f28ff004db2703201a03a1ea347fcd5
wesley <wesleyaleixo0950@gmail.com>
2023-12-12 16:55:54 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ class App {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Origin",
+        "https://amigo-secreto-react-next.vercel.app"
+      );
       next();
     });
   }
