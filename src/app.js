cf79f3d92d16979cde3b90e88096adf834051429
wesley <wesleyaleixo0950@gmail.com>
2023-12-12 17:29:31 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ class App {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Origin",
+        "https://amigo-oculto-neon.vercel.app/"
+      );
       res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
       res.header(
         "Access-Control-Allow-Headers",
